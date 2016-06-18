67c426c397d464f8c4df78bc617341f6be0b76a8
adaschevici <a.daschevici@gmail.com>
2016-06-18 10:43:05 +0100

    Distributed examples

---
+++ chap7/distributed_star.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Subscriber struct {
+	Addres     net.Addr
+	Connection net.Conn
+	do         chan Task
+}
+
+type Task struct {
+	name string
+}
+
+var SubscriberCount int
+var Subscribers []Subscriber
+var CurrentSubscriber int
+var taskChannel chan Task
+
+func (sb Subscriber) awaitTask() {
+	select {
+	case t := <-sb.do:
+		fmt.Println("Performing task:", t.name)
+	}
+}
+
+func serverListen(listener net.Listener) {
+	for {
+		conn, _ := listener.Accept()
+
+		SubscriberCount++
+
+		subscriber := Subscriber{Address: conn.remoteAddr(), Connection: conn}
+
+		subscriber.do = make(chan Task)
+
+		subscriber.awaitTask()
+
+		_ = append(Subscribers, subscriber)
+	}
+}
+
+func doTask() {
+	for {
+		select {
+		case task := <-taskChannel:
+			fmt.Println(task.Name, "invoked")
+			Subscribers[CurrentSubscriber].do <- task
+			if (CurrentSubscriber + 1) > SubscriberCount {
+				CurrentSubscriber = 0
+			} else {
+				CurrentSubscriber++
+			}
+		}
+	}
+}
+
+func main() {
+
+	destinationStatus := make(chan int)
+
+	SubscriberCount = 0
+	CurrentSubscriber = 0
+
+	taskChannel = make(chan Task)
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println("Could not start server!", err)
+	}
+	go serverListen(listener)
+	go doTask()
+
+	<-destinationStatus
+}
