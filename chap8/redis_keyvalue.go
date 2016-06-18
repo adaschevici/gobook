f8b8e611bb14ce25dd21903a34f905cfcc6df6ae
adaschevici <a.daschevici@gmail.com>
2016-06-18 12:58:05 +0100

    Add working redis without pools

--- chap8/redis_keyvalue.go
+++ chap8/redis_keyvalue.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/garyburd/redigo/redis"
 )
 
@@ -12,21 +12,35 @@ func main() {
 	connection, _ := redis.Dial("tcp", ":6379")
 	defer connection.Close()
 
-	data, err := redis.Values(connection.Do("SORT", "Users", "BY", "User:*->name",
-		"GET", "User:*->name"))
+	// data, err := redis.Values(connection.Do("SORT", "Users", "BY", "User:*->name",
+	// 	"GET", "User:*->name"))
+
+	// if err != nil {
+	// 	fmt.Println("Error getting values", err)
+	// }
+
+	// spew.Dump(data)
+	// for _ = range data {
+	// 	var Uname string
+	// 	data, err = redis.Scan(data, &Uname)
+	// 	if err != nil {
+	// 		fmt.Println("Error getting value", err)
+	// 	} else {
+	// 		fmt.Println("Name Uname")
+	// 	}
+	// }
+	data, err := connection.Do("INFO")
 
 	if err != nil {
-		fmt.Println("Error getting values", err)
+		fmt.Println("Error connecting to redis.")
+	} else {
+		log.Printf("info=%s", data)
 	}
 
-	spew.Dump(data)
-	for _ = range data {
-		var Uname string
-		data, err = redis.Scan(data, &Uname)
-		if err != nil {
-			fmt.Println("Error getting value", err)
-		} else {
-			fmt.Println("Name Uname")
-		}
-	}
+	inserted, _ := connection.Do("SET", "awesome", "I inserted some crap")
+	log.Printf("Inserted=%s", inserted)
+
+	getme, _ := connection.Do("GET", "awesome")
+	log.Printf("Got: %s", getme)
+
 }
