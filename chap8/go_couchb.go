1733dc7a0953e65604c4acac8de9a0f0c74f68d4
adaschevici <a.daschevici@gmail.com>
2016-06-19 14:18:01 +0100

    Add couchbase example with operations

--- chap8/go_couchb.go
+++ chap8/go_couchb.go
@@ -20,5 +20,12 @@ func main() {
 	if err != nil {
 		fmt.Println("ERRROR OPENING BUCKET:", err)
 	}
+	value := "test value"
+	cas, _ := bucket.Insert("document_name", &value, 0)
+	fmt.Printf("Inserted document CAS is `%08x`\n", cas)
+
+	var someValue interface{}
+	scas, _ := bucket.Get("document_name", &someValue)
+	fmt.Printf("Got value `%+v` with CAS `%08x`\n", value, scas)
 	fmt.Println("Example Successful - Exiting")
 }
