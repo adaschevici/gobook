be0c4032b825bceed7f0688ab4fc34a5dd2d0da0
adaschevici <a.daschevici@gmail.com>
2016-06-20 01:06:12 +0100

    Add viewquery but this does not work still

--- chap8/go_couchb.go
+++ chap8/go_couchb.go
@@ -16,16 +16,27 @@ func main() {
 		fmt.Println("ERRROR CONNECTING TO CLUSTER:", err)
 	}
 	// Open Bucket
-	bucket, err = cluster.OpenBucket("travel-sample", "")
+	bucket, err = cluster.OpenBucket("beer-sample", "")
 	if err != nil {
 		fmt.Println("ERRROR OPENING BUCKET:", err)
 	}
-	value := "test value"
-	cas, _ := bucket.Insert("document_name", &value, 0)
-	fmt.Printf("Inserted document CAS is `%08x`\n", cas)
+	// value := "test value"
+	// cas, _ := bucket.Insert("document_name", &value, 0)
+	// fmt.Printf("Inserted document CAS is `%08x`\n", cas)
 
-	var someValue interface{}
-	scas, _ := bucket.Get("document_name", &someValue)
-	fmt.Printf("Got value `%+v` with CAS `%08x`\n", value, scas)
+	// var someValue interface{}
+	// scas, _ := bucket.Get("document_name", &someValue)
+	// fmt.Printf("Got value `%+v` with CAS `%08x`\n", value, scas)
+
+	vq := gocb.NewViewQuery("dev_beer", "by_name") //.Skip(6).Limit(3)
+	rows, err := bucket.ExecuteViewQuery(vq)
+
+	if err := rows.Close(); err != nil {
+		fmt.Printf("View query error: %s\n", err)
+	}
+	var row interface{}
+	for rows.Next(&row) {
+		fmt.Printf("Got row `%+v`\n")
+	}
 	fmt.Println("Example Successful - Exiting")
 }
