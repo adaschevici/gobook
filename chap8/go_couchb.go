02b640ea44d80aadb277fb74690c2fd1872e89a4
adaschevici <a.daschevici@gmail.com>
2016-06-21 20:40:18 +0100

    Now it works, need to start docker container both on 8092, 8091

--- chap8/go_couchb.go
+++ chap8/go_couchb.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/couchbase/gocb"
+	"github.com/davecgh/go-spew/spew"
+	// "gopkg.in/couchbaselabs/gocb.v1"
 )
 
 // bucket reference - reuse as bucket reference in the application
@@ -20,23 +22,28 @@ func main() {
 	if err != nil {
 		fmt.Println("ERRROR OPENING BUCKET:", err)
 	}
-	// value := "test value"
-	// cas, _ := bucket.Insert("document_name", &value, 0)
-	// fmt.Printf("Inserted document CAS is `%08x`\n", cas)
+	value := "test value"
+	cas, _ := bucket.Insert("document_name", &value, 0)
+	fmt.Printf("Inserted document CAS is `%08x`\n", cas)
 
-	// var someValue interface{}
-	// scas, _ := bucket.Get("document_name", &someValue)
-	// fmt.Printf("Got value `%+v` with CAS `%08x`\n", value, scas)
+	var someValue interface{}
+	scas, _ := bucket.Get("document_name", &someValue)
+	fmt.Printf("Got value `%+v` with CAS `%08x`\n", value, scas)
 
-	vq := gocb.NewViewQuery("dev_beer", "by_name") //.Skip(6).Limit(3)
+	vq := gocb.NewViewQuery("beer", "by_name").Limit(2)
 	rows, err := bucket.ExecuteViewQuery(vq)
 
-	if err := rows.Close(); err != nil {
-		fmt.Printf("View query error: %s\n", err)
+	if err != nil {
+		spew.Dump(err)
 	}
+
 	var row interface{}
 	for rows.Next(&row) {
+		spew.Dump(row)
 		fmt.Printf("Got row `%+v`\n")
+	}
+	if err := rows.Close(); err != nil {
+		fmt.Printf("View query error: %s\n", err)
 	}
 	fmt.Println("Example Successful - Exiting")
 }
