66b14c2a1b3283dab8a878612ecd824b893497b6
adaschevici <a.daschevici@gmail.com>
2016-06-28 21:51:17 +0100

    Add logging to files on goroutines

--- chap9/logoreas.go
+++ chap9/logoreas.go
@@ -5,13 +5,39 @@ import (
 	"log"
 	"os"
 	"os/user"
+	"strconv"
 )
 
+var totalGoroutines = 5
+
+type Worker struct {
+	FileName string
+	File     *os.File
+	wLog     *log.Logger
+	Name     string
+}
+
 func main() {
+	done := make(chan bool)
 	someUser, _ := user.Current()
 	currentUser := someUser.Username
 	var listenFolder = fmt.Sprintf("/Users/%s/couchbase_volume/", currentUser)
 	logFile, _ := os.OpenFile(fmt.Sprintf("%stest.log", listenFolder), os.O_RDWR, 0755)
+	for i := 0; i < totalGoroutines; i++ {
+
+		myWorker := Worker{}
+		myWorker.Name = "Goroutine " + strconv.FormatInt(int64(i), 10) + ""
+		myWorker.FileName = listenFolder + strconv.FormatInt(int64(i), 10) + ".log"
+		tmpFile, _ := os.OpenFile(myWorker.FileName, os.O_CREATE, 0755)
+		myWorker.File = tmpFile
+		myWorker.wLog = log.New(myWorker.File, myWorker.Name, 1)
+		go func(w *Worker) {
+
+			w.wLog.Print("Hmm")
+
+			done <- true
+		}(&myWorker)
+	}
 
 	log.SetOutput(logFile)
 	log.Println("Sending an entry to log!")
